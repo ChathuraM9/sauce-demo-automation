ddc21b78f4be747679e0d99218b8b0dc3b5166b7
Chathura Madhushan <88130369+ChathuraM9@users.noreply.github.com>
2024-07-29 18:27:39 +0530

    Update emptyLoginTest.js

--- emptyLoginTest.js
+++ emptyLoginTest.js
@@ -11,14 +11,7 @@ const chrome = require('selenium-webdriver/chrome');
         await driver.findElement(By.id('user-name')).sendKeys('');
         await driver.findElement(By.id('password')).sendKeys('');
         await driver.findElement(By.className('submit-button')).click();
-
-       
-        let currentUrl = await driver.getCurrentUrl();
-        if (currentUrl === 'https://www.saucedemo.com/') {
-            console.log('User is still on the login page, access to products page is denied.');
-        } else {
-            console.error('User was able to access the products page, test failed.');
-        }
+        
     } catch (error) {
         console.error('Test failed:', error);
     } finally {
